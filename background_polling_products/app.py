380d6614881c71e8827946ed6282382c22692ba9
Fariha Mehzabin <henlothere45@gmail.com>
2023-04-30 23:24:04 +0600

    make a separate server for background polling products

---
+++ background_polling_products/app.py
@@ -0,0 +1,41 @@
+from flask import Flask
+import time
+import mysql.connector
+
+config = {
+    "DEBUG": True,  # some Flask specific configs
+}
+
+app = Flask(__name__)
+
+def delete_expired_reservations():
+    # Replace with your own database credentials
+    cnx = mysql.connector.connect(host="localhost", user="root", password="password", database="ecommerce")
+    cursor = cnx.cursor()
+
+    # Call the stored procedure to delete expired reservations
+    cursor.callproc("delete_expired_reservations")
+    result = None
+    for row in cursor.stored_results():
+        result = row.fetchone()[0]
+        
+    print(result)
+
+    if result == 0:
+        cnx.commit()
+    else:
+        cnx.rollback()
+
+    cursor.close()
+    cnx.close()
+
+def run_background_script():
+    while True:
+        print("Running background script...")
+        delete_expired_reservations()
+        time.sleep(60)  # Wait for 60 seconds before running again
+
+
+if __name__ == "__main__":
+    run_background_script()
+    app.run(host='0.0.0.0', port=8081)
--- ecom_api/app.py
+++ ecom_api/app.py
@@ -5,7 +5,7 @@ from ecom_api.routes.public_users import public_users_routes
 from ecom_api.routes.company_users import company_users_routes
 from ecom_api.routes.categories import categories_routes
 from ecom_api.routes.products import products_routes
-from ecom_api.background_polling import BackgroundPolling
+
 
 config = {
     "DEBUG": True,  # some Flask specific configs
@@ -19,13 +19,7 @@ company_users_routes(app)
 categories_routes(app)
 products_routes(app)
 
-bg_polling = BackgroundPolling()
-
-def start_background_script():
-    thread = threading.Thread(target=bg_polling.run_background_script, daemon=True)
-    thread.start()
 
 
 if __name__ == "__main__":
-    start_background_script()
     app.run(host='0.0.0.0', port=8080)
